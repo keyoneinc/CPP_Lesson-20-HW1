97734692b162c07a370be9acc77be59275bafe8f
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 03:45:22 +0100

    added Students class methods defenition in Student.cpp

--- hw20_1/Student.cpp
+++ hw20_1/Student.cpp
@@ -1,1 +1,82 @@
 #include "Student.h"
+
+Student::Student(string fullName, double averageScore, string phoneNumber, string address, string& institutionName, int groupNumber)
+{
+	this->fullName = fullName;
+	this->averageScore = averageScore;
+	this->phoneNumber = phoneNumber;
+	this->address = address;
+	this->institutionName = institutionName;
+	this->groupNumber = groupNumber;
+}
+
+string Student::getFullName() const
+{
+	return fullName;
+}
+
+double Student::getAverageScore() const
+{
+	return averageScore;
+}
+
+string Student::getPhoneNumber() const
+{
+	return phoneNumber;
+}
+
+string Student::getAddress() const
+{
+	return address;
+}
+
+string Student::getInstitutionName() const
+{
+	return institutionName;
+}
+
+int Student::getGroupNumber() const
+{
+	return groupNumber;
+}
+
+void Student::setFullName(const string& fullName)
+{
+	this->fullName = fullName;
+}
+
+void Student::setAverageScore(double averageScore)
+{
+	this->averageScore = averageScore;
+}
+
+void Student::setPhoneNumber(const string& phoneNumber)
+{
+	this->phoneNumber = phoneNumber;
+}
+
+void Student::setAddress(const string& address)
+{
+	this->address = address;
+}
+
+void Student::setInstitutionName(const string& institutionName)
+{
+	this->institutionName = institutionName;
+}
+
+void Student::setGroupNumber(int groupNumber)
+{
+	this->groupNumber = groupNumber;
+}
+
+void Student::showInfo()
+{
+	cout << "\nStudent Information:" << endl;
+	cout << "Full Name: " << fullName << endl;
+	cout << "Average Score: " << averageScore << endl;
+	cout << "Phone Number: " << phoneNumber << endl;
+	cout << "Address: " << address << endl;
+	cout << "Institution Name: " << institutionName << endl;
+	cout << "Group Number: " << groupNumber << endl << endl;
+}
