916cea8dc2a74a24a7a9618a31ecbd99aefe361b
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 03:42:48 +0100

    created class Student, added the class to Student header file, added getters and setters, also with showinfo method

---
+++ hw20_1/Student.h
@@ -0,0 +1,35 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Student {
+private:
+    string fullName;
+    double averageScore;
+    string phoneNumber;
+    string address;
+    string institutionName;
+    int groupNumber;
+
+public:
+    Student(string fullName, double averageScore, string phoneNumber, string address,string& institutionName, int groupNumber);
+    
+    string getFullName() const;
+    double getAverageScore() const;
+    string getPhoneNumber() const;
+    string getAddress() const;
+    string getInstitutionName() const;
+    int getGroupNumber() const;
+
+    void setFullName(const string& fullName);
+    void setAverageScore(double averageScore);
+    void setPhoneNumber(const string& phoneNumber);
+    void setAddress(const string& address);
+    void setInstitutionName(const string& institutionName);
+    void setGroupNumber(int groupNumber);
+
+    void showInfo();
+
+
+};
