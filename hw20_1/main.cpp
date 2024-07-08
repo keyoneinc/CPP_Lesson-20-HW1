eb02f55fcef80fd86657501442989240029184c2
Yaroslav <yaroslav@keyoneinc.uk>
2024-07-08 03:54:08 +0100

    added the test code to main.cpp, program isn't working there's some error

--- hw20_1/main.cpp
+++ hw20_1/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
+#include "Student.h"
 
 int main() {
+    Student student1("Yaroslav", 95, "+123456789", "Street, City, Zipcode", "Kharkiv NTU University", 222);
+
+    student1.showInfo();
+
+    student1.setAverageScore(97);
+    student1.setPhoneNumber("+132112414");
+
+    cout << "\nUpdated Student Information:" << endl;
+    student1.showInfo();
+
+    return 0;
+
 
 }
